3c7fad34dfa6607a6e1e501ec42750d07c7accb8
Christopher Young <chris@chrisyoung.net>
2013-04-11 20:56:44 -0400

    Add warnings to ghc --make and bail on script if ghc fails

--- site/dev.sh
+++ site/dev.sh
@@ -1,3 +1,8 @@
-ghc --make site.hs
-./site rebuild
-./site preview
+#!/bin/bash
+ghc --make -fwarn-unused-imports -fwarn-unused-binds -fwarn-overlapping-patterns -fwarn-missing-signatures -fwarn-hi-shadowing -fwarn-missing-fields -fwarn-missing-methods site.hs
+
+if [ $? -eq 0 ]
+then
+    ./site rebuild
+    ./site preview
+fi
