73470b03246eaf734a31de4d0bb2eb01d4045144
Christopher Young <chris@chrisyoung.net>
2013-04-24 14:06:32 -0400

    Add js for simple touch ups

---
+++ site/js/touch-ups.js
@@ -0,0 +1,17 @@
+;(function ($) {
+
+    var centerFrontbox = function () {
+        var w = $(window);
+        var el = $(".frontbox");
+        el.css({
+            'position':'absolute',
+            'top':Math.abs(((w.height() - el.outerHeight()) / 2) + w.scrollTop()),
+            'left':Math.abs(((w.width() - el.outerWidth()) / 2) + w.scrollLeft())
+        });
+    };
+
+    $(document).ready(function($){
+        centerFrontbox();
+    });
+
+}($));
