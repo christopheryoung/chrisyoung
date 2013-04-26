a0a97a0a3ee55e6563274c753931e5c304c6179f
Christopher Young <chris@chrisyoung.net>
2013-04-26 11:48:11 -0400

    Add deploy script

---
+++ site/deploy.sh
@@ -0,0 +1,14 @@
+
+ghc --make site.hs
+
+if [ $? -eq 0 ]
+then
+    ./site rebuild
+fi
+
+if [ $? -eq 0 ]
+then
+    rsync --delete --verbose --recursive --dry-run _site/* chrisyoung@chrisyoung.net:~/webapps/static
+else
+    echo "rsync failed!"
+fi
