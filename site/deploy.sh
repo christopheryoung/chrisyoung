38c46bd408c09f73761e31b6eab335a724c1a43d
Christopher Young <chris@chrisyoung.net>
2013-05-03 14:49:58 -0400

    Make sure that rsync captures .htaccess

--- site/deploy.sh
+++ site/deploy.sh
@@ -8,7 +8,8 @@ fi
 
 if [ $? -eq 0 ]
 then
-    rsync --delete --verbose --recursive _site/* chrisyoung@chrisyoung.net:~/webapps/static
+    (shopt -s dotglob; rsync --delete --verbose --recursive _site/* chrisyoung@chrisyoung.net:~/webapps/static)
+
 else
     echo "rsync failed!"
 fi
