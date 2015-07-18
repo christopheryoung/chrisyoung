44ec2ef6684a81e05a9b618297bda6ede05a648c
Christopher Young <chris@chrisyoung.net>
2015-07-18 15:09:29 -0400

    Fix bad file name in script

--- site/preview.sh
+++ site/preview.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ghc --make -fwarn-unused-imports -fwarn-unused-binds -fwarn-overlapping-patterns -fwarn-missing-signatures -fwarn-hi-shadowing -fwarn-missing-fields -fwarn-missing-methods site.hs
+ghc --make -fwarn-unused-imports -fwarn-unused-binds -fwarn-overlapping-patterns -fwarn-missing-signatures -fwarn-hi-shadowing -fwarn-missing-fields -fwarn-missing-methods Main.hs
 
 if [ $? -eq 0 ]
 then
