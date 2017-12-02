e5b9a9ecc372d7da941ca8b62ba765eb46eb4bf7
Christopher Young <christopher.young@schrodinger.com>
2017-12-02 11:17:22 -0500

    Remove old preview script

--- site/preview.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ghc --make -fwarn-unused-imports -fwarn-unused-binds -fwarn-overlapping-patterns -fwarn-missing-signatures -fwarn-hi-shadowing -fwarn-missing-fields -fwarn-missing-methods Main.hs
-
-if [ $? -eq 0 ]
-then
-    ./site rebuild
-    ./site watch
-fi
